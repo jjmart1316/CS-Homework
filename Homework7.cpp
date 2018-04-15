20c5b15cdb58a1bbc4accf6daf4141792001e7a8
Juan J. Martinez <jjmart1316@gmail.com>
2018-04-15 15:23:44 -0500

    Update Homework7.cpp

--- Homework7.cpp
+++ Homework7.cpp
@@ -4,33 +4,21 @@
  * Description: Reverses a c-string
  */
 #include <iostream>
+#include <cstring>
 using namespace std;
 
 void reverseString(char *a);
 //precondition: Takes a cstring by their location
 //postcondition:: calls lastCharIndex() to locate the last character and reverses the string
-int lastCharIndex(char *a);
-//precondition: takes a cstring by their location
-//postcondition: returns the index of the last typed character in the string
 
 int main() {
-
+    
     char str[101];
-
     cout << "Type something: ";
     cin.getline(str, 101);
     reverseString(str);
-
     cout << str;
     return 0;
-}
-
-int lastCharIndex(char *a) {
-    int i = 0;
-    while (a[i] != NULL) {
-        i++;
-    }
-    return i;
 }
 
 void reverseString(char *a) {
@@ -38,7 +26,7 @@ void reverseString(char *a) {
     charPointer pStart, pEnd;
 
     pStart = a;
-    pEnd = a + lastCharIndex(a) - 1;
+    pEnd = a + strlen(a) - 1;
 
     while(pStart < pEnd) {
         char temp = *pStart;
