565c74430c74c6601eb7ab2c45d021cff7a0dd02
Juan J. Martinez <jjmart1316@gmail.com>
2018-05-03 12:26:30 -0500

    Add files via upload

--- project7.cpp
+++ project7.cpp
@@ -3,15 +3,12 @@
  * Email Address: mart1316@pnw.edu
  * Assignment Number: Homework 4: Chapter 6, Question 2
  * Description: Program copies data from a file, then computes and transfers that data to a new file
- * Last Changed: March 4, 2018
+ * Last Changed: March 14, 2018
  */
 
 #include <iostream>
 #include <fstream>
 using namespace std;
-
-void totalScore(ifstream& fin, ofstream& fout);
-void names(ifstream& fin, ofstream& fout);
 
 int main() {
     ifstream fin;
@@ -29,31 +26,30 @@ int main() {
         cout << "Output file opening failed." << endl;
         exit(1);
     }
+    string lName, fName;
+    int score, total;
 
-    names(fin, fout); //Calls a function to copy names and transfers them to an output-file
-    totalScore(fin, fout); //Calls to compute scores and transfers data to an output-file
+    while(fin >> lName >> fName) {
+        //Loop collects name's of students along with data
+        cout << lName << " " << fName << " ";
+        fout << lName << " " << fName << " ";
+        score = 0; total = 0;
+        for(int i = 0; i < 10; i++){
+            //Loop collects scores from file, adds them together, and print the average score per student
+            fin >> score;
+            cout << score << " ";
+            fout << score << " ";
+            total += score;
+        }
+        cout << total / 10.0;
+        fout << total / 10.0;
+        cout << endl;
+        fout << endl;
+    }
 
     fin.close();
     fout.close();
 
     cout << "Program completed."; //prints out statement when program completes tasks
     return 0;
-}
-
-void names(ifstream& fin, ofstream& fout) {
-    //Copies the last name and first name from a file and transfers them to the output file
-    string lastName, firstName;
-    fin >> lastName >> firstName;
-    fout << lastName << " " << firstName << " ";
-}
- void totalScore(ifstream& fin, ofstream& fout) {
-     //Copies the scores, adds them, finds the average score from the addition and transfers the data to the output file
-     double score, totalSum = 0;
-     int counter = 0;
-     while(fin >> score) {
-        fout << score << " ";
-        totalSum += score;
-        counter++;
-     }
-     fout << totalSum / counter << endl;
 }
